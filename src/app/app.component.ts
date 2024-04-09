39a89b6a53548e56f8558c62682eec9228523392
robertpalm <robert.rp.palm@gmail.com>
2024-04-09 20:55:11 +0300

    Next steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'application-form';
+
+  //TODO: 1 https://angular.io/guide/form-validation
+  //TODO: 2 Work experience custom validator to check, that it's a number & 1 number after comma.
+  //TODO: 3 If form input invalid, there should appear an error text under it.
+  //TODO: 4 Invalid "SUBMIT" message
+  //TODO: 5 "RESET" clear all fields & errors
+  //TODO: 6 "RESET" success message
+  //TODO: 7 Message as a reusable component that takes input props (text, type, color)
+  //TODO: 8 Invalid input error text should appear/disappear on blur
 }
